47fa58b012276e45da5fb981a5a236f52ab52009
2526356 <s9jnmart@stud.uni-saarland.de>
2014-10-27 19:18:03 +0100

    hough transform removed (now located in NuLLImage repo)

--- main.cpp
+++ main.cpp
@@ -10,13 +10,14 @@
 //#include "NuLLProcessing.inl"
 //#include "NuLLTools.inl"
 
-#include "TestCases.h"
+//#include "TestCases.h"
+#include "NuLLTransform.inl"
 
 
 /*
  * NuLL: Numerical Lightweight Library
  * use for: solving linear systems, matrix decomposition,
- *          eigenvalue decomposition, data procressing
+ *          eigenvalue decomposition, data processing
  */
 
 int main()
@@ -26,14 +27,23 @@ int main()
     A(0,1)=5;   A(1,1)=7;
     A.print();
 
-    VectorD b(2);
+    MatrixD B(2,2);
+    MatrixD C(2,2);
+
+    NuLLTransform::fourierTransform(A,B,C);
+    B.print();
+
+    //NuLLTransform::cosineTransform(B,A);
+    //A.print();
+
+    /*VectorD b(2);
     b[0]=11;
     b[1]=13;
     b.print();
 
     VectorD x(2);
     x[0]=1;
-    x[1]=1;
+    x[1]=1;*/
 
     /*MatrixD A(3,3);
     A(0,0)=2;   A(1,0)=0;   A(2,0)=1;
@@ -53,7 +63,7 @@ int main()
     x[2]=0;*/
 
     //solution = 7.111, -3.222
-    NuLLSolve::gaussSeidelSOR(A, b, x);
+    /*NuLLSolve::gaussSeidelSOR(A, b, x);
     x.print();
 
     MatrixD tmp(2,2);
@@ -69,7 +79,7 @@ int main()
     double lambdaInv = 1. / lambda;
     x = A * v;
     x *= lambdaInv;
-    x.print();
+    x.print();*/
 
 
 
